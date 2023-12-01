4eb5f1db2fd0652c14b89c98a2ec8485149558a1
fabio.kfouri <fabio.kfouri@ubirata.ai>
2023-12-01 16:19:29 -0300

    Rich enviroment

--- syncro_git/cli.py
+++ syncro_git/cli.py
@@ -6,6 +6,9 @@ from pathlib import Path
 
 import click
 from git import Repo  # pip install gitpython
+from rich.console import Console
+
+
 
 THIS_PATH = Path(dirname(sys.executable)) if getattr(sys, "frozen", False) else Path(dirname(__file__))
 ROOT_PAHT = THIS_PATH.parents[0]
@@ -17,29 +20,36 @@ FOLDER_NAME = ".temp-dna-engineer-template"
 ORIGIN = "git@gitlab.gerdau.digital:Industrial/gemeosupplychaintmecpin-lakehouse.git"
 FOLDER_NAME = "gemeosupplychaintmecpin-lakehouse"
 DESTINY = "git@gitlab.ubirata.ai:templates_repo/test.git"
+DESTINY = "git@gitlab.ubirata.ai:tmec/lakehouse.git"
 
 REMOTE_CLONE = "origin_clone"
 
+console = Console(style="yellow")
 
 @click.command()
 @click.option("--origin", "-o", required=True, default=ORIGIN)
 @click.option("--folder", "-f", required=True, default=FOLDER_NAME)
 @click.option("--branch", "-b", required=False, default=None)
-@click.option("--detiny", "-d", required=False, default=DESTINY)
-def app(origin, folder, branch, detiny):
+@click.option("--destiny", "-d", required=False, default=DESTINY)
+def app(origin, folder, branch, destiny):
     target_dir = get_target_dir(folder)
+
+    console.print(f"""
+Path to be used as bridge: [purple]{folder}[/]
+origin: [purple]{origin}[/]
+{REMOTE_CLONE}: [purple]{destiny}[/] """)
 
     repo = None
     if target_dir.exists():
         try:
-            click.echo(f"Read current path {target_dir}")
             repo = Repo(target_dir)
+            console.print(f"Clone from [blue]{origin}[/b]")
         except Exception as e:
-            click.echo(f"It's not a valid path. The address '{target_dir}' will be recreated.")
+            console.print(f"It's not a valid path. The address [red]{target_dir}[/] will be recreated.", style="red")
             shutil.rmtree(target_dir)
 
     if repo is None:
-        click.echo(f"Clone from {origin}")
+        console.print(f"Clone from [blue]{origin}[/]")
         repo = Repo.clone_from(url=origin, to_path=str(target_dir), branch=branch)
 
     repo = get_all_branches(repo)
@@ -50,7 +60,7 @@ def app(origin, folder, branch, detiny):
     heads = [r.path for r in repo.heads]
 
     if REMOTE_CLONE not in remotes:
-        repo.create_remote(name=REMOTE_CLONE, url=detiny)
+        repo.create_remote(name=REMOTE_CLONE, url=destiny)
         remotes.append(REMOTE_CLONE)
 
     for remote in repo.remotes:
@@ -63,34 +73,14 @@ def app(origin, folder, branch, detiny):
     # log.append(git.remote("-v"))
     # log.append(git.branch('-v', '-a'))
 
-    git.remote
-
-    # remote.push()
-
     for branch in repo.branches:
-        refspec = "{}:{}".format(branch, branch)
-        click.echo(f"Push {refspec} to {DESTINY}")
-        remote.push(refspec=refspec)
+        if 'feature' not in branch.name:
+            refspec = "{}:{}".format(branch, branch)
+            console.print(f"Push [bold blue]{branch}[/] to [bold blue]{REMOTE_CLONE}[/]")
+            remote.push(refspec=refspec)
 
-    repo = repo
+    console.print(":thumbs_up: Done !!!")
 
-    # if not target_dir.exists():
-    #     click.echo("The target directory doesn't exist")
-    #     raise SystemExit(1)
-
-    # for entry in target_dir.iterdir():
-    #     click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
-
-    # click.echo()
-
-
-# FOLDER = ROOT_PAHT.joinpath(FOLDER_NAME)
-# if FOLDER.exists:
-#     shutil.rmtree(FOLDER)
-
-# FOLDER.mkdir(exist_ok=True)
-
-#
 
 
 def get_target_dir(folder):
@@ -106,7 +96,7 @@ def get_all_branches(repo: Repo):
     """
     for b in repo.remote(name="origin").fetch():
         try:
-            click.echo(f"Get branch from {b.name}")
+            console.print(f"Get branch from [bold blue]{b.name}[/]")
             repo.git.checkout("-B", b.name.split("/")[1], b.name)
         except: ...
     return repo
